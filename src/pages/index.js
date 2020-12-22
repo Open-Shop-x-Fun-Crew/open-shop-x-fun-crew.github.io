fbbebc4d7e6f7ed40b7e1bc3e1ed0aaaf1318762
Sofia Darke <sofia.darke@hyperisland.se>
2020-12-22 13:50:40 +0100

    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
 
   return (
     <>
-      <p>old paragraph text</p>
+      <p>old paragsdfdraph text</p>
       <ClickMeButton onClick={toggleButton} isClicked={clicked} />
     </>
   )
