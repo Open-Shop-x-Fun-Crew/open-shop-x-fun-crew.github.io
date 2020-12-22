5fc9e150b55de523c1676d9d3df657511efa0cfd
nehahirve <neha.hirve@hyperisland.se>
2020-12-22 21:10:33 +0100

    cursor follows mouse

--- src/components/logo-button.js
+++ src/components/logo-button.js
@@ -4,7 +4,11 @@ import styles from './logo-button.module.scss'
 
 export default function LogoButton(props) {
   return (
-    <button className={styles.button} onClick={props.onClick}>
+    <button
+      className={styles.button}
+      onClick={props.onClick}
+      style={{ top: props.pos.y, left: props.pos.x }}
+    >
       open
       <br />
       shop.
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,17 +3,28 @@ import React, { useState } from 'react'
 import LogoButton from '../components/logo-button'
 import Splash from '../components/splash'
 import Site from '../components/site'
+import { useEffect } from 'react'
 
 export default function Home() {
   const [splashVisible, setSplashVisible] = useState(true)
+  const [pos, setPos] = useState({ x: null, y: null })
+
+  function updatePos(e) {
+    setPos({ x: e.clientX, y: e.clientY })
+  }
 
   function toggleSplashVisible() {
     setSplashVisible(!splashVisible)
   }
 
+  useEffect(() => {
+    window.addEventListener('mousemove', updatePos)
+    return () => window.removeEventListener('mousemove', updatePos)
+  }, [])
+
   return (
     <div>
-      <LogoButton onClick={toggleSplashVisible} />
+      <LogoButton onClick={toggleSplashVisible} pos={pos} />
       <Splash isVisible={splashVisible} />
       <Site isVisible={!splashVisible} />
     </div>
