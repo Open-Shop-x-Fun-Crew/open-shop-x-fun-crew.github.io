e4bc24d5f93c35f95403d262ca7ea8371cd5346f
Saeid Fattahi <70689949+saeidfattahi@users.noreply.github.com>
2020-12-22 15:39:58 +0100

    structure defined

---
+++ src/components/site.js
@@ -0,0 +1,6 @@
+import React, { useState } from 'react'
+import LogoButton from '../components/site'
+
+export default function Site() {
+  return null
+}
---
+++ src/components/splash.js
@@ -0,0 +1,6 @@
+import React, { useState } from 'react'
+import LogoButton from '../components/splash'
+
+export default function Splash() {
+  return null
+}
