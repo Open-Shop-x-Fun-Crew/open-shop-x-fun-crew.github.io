f59ee5b3ea153fb816b62dba9247fa7758da5cf6
Emmy_codes <alunia.moon@gmail.com>
2020-12-23 19:02:43 +0100

    site html additions

--- src/components/site.js
+++ src/components/site.js
@@ -8,9 +8,23 @@ export default function Site(props) {
     return (
       <div>
         <main>
+          <figure>
+            <img></img>
+            <figcaption>Martin 1985.</figcaption>
+          </figure>
+          <section>
+            <button>kick up some dust</button>
+            <p className={styles.hidden}>info@openshop.se</p>
+          </section>
           <div>
             <p>Open Shop is film-making duo</p>
           </div>
+          <figure>
+            <img></img>
+            <figcaption>Viktor 1993.</figcaption>
+          </figure>
+          <a href="https://www.instagram.com/artandpoeticjustice/?hl=en" className={styles.links}>slow-skate with us</a>
+          <button>look here</button>
           <FilmList />
         </main>
         <footer>
