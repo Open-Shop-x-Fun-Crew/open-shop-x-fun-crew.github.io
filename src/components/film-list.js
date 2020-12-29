28901b8c347dfac9ebdf06fef5babc3cc84aefe3
nehahirve <neha.hirve@hyperisland.se>
2020-12-29 14:15:17 +0100

    updated folder paths

--- src/components/film-list.js
+++ src/components/film-list.js
@@ -1,17 +1,32 @@
 import React from 'react'
 import FilmListItem from './film-list-item'
-import Distressed from '../images/Distressed.jpg'
-import DimmigaDar from '../images/DimmigaDar.jpg'
-import MadDog from '../images/MadDog.jpg'
+import Distressed from '../../static/images/Distressed.jpg'
+import DimmigaDar from '../../static/images/DimmigaDar.jpg'
+import MadDog from '../../static/images/MadDog.jpg'
 
 import styles from './film-list.module.scss'
 
 export default function FilmList(props) {
   return (
     <ul>
-      <FilmListItem image={Distressed} text="Distressed" description="Short film 2020, 15min" alt="Description of film" />
-      <FilmListItem image={DimmigaDar} text="Dimmiga Dar" descrption="Short film 2020, 17min" alt="Description of film" />
-      <FilmListItem image={MadDog} text="Mad Dog" description="Full-length film 2016, 85min" alt="Description of film" />
+      <FilmListItem
+        image={Distressed}
+        text="Distressed"
+        description="Short film 2020, 15min"
+        alt="Description of film"
+      />
+      <FilmListItem
+        image={DimmigaDar}
+        text="Dimmiga Dar"
+        descrption="Short film 2020, 17min"
+        alt="Description of film"
+      />
+      <FilmListItem
+        image={MadDog}
+        text="Mad Dog"
+        description="Full-length film 2016, 85min"
+        alt="Description of film"
+      />
     </ul>
   )
 }
