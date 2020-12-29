df8b79a29cbe6bc3dd347ad4315088de521e738c
nehahirve <neha.hirve@hyperisland.se>
2020-12-29 14:01:57 +0100

    cleanup

--- src/components/site.js
+++ src/components/site.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import FilmList from './film-list'
-
+import martin from '../../static/images/martin.png'
+import viktor from '../../static/images/viktor.png'
 import styles from './site.module.scss'
 
 export default function Site(props) {
@@ -9,7 +10,10 @@ export default function Site(props) {
       <div>
         <main>
           <figure>
-            <img src="../images/MardinKid.png" alt="Picture of Martin as a childm with his father, on the beach." />
+            <img
+              src={martin}
+              alt="Picture of Martin as a child with his father, on the beach."
+            />
             <figcaption>Martin 1985.</figcaption>
           </figure>
           <section>
@@ -17,14 +21,24 @@ export default function Site(props) {
             <p className={styles.hidden}>info@openshop.se</p>
           </section>
           <div>
-            <p>Open Shop is film-making duo Martin Sandin &amp; Viktor Annerstål. 
-              Martin writes and directs, Viktor edits and shoots</p>
+            <p>
+              Open Shop is film-making duo Martin Sandin &amp; Viktor Annerstål.
+              Martin writes and directs, Viktor edits and shoots
+            </p>
           </div>
           <figure>
-            <img src="../images/ViktorBaby.png" alt="Picture of Viktor as a baby, with his father in their living room. Dad is taking a photo of Martin with an old style camera." />
+            <img
+              src={viktor}
+              alt="Picture of Viktor as a baby, with his father in their living room. Dad is taking a photo of Martin with an old style camera."
+            />
             <figcaption>Viktor 1993.</figcaption>
           </figure>
-          <a href="https://www.instagram.com/artandpoeticjustice/?hl=en" className={styles.links}>slow-skate with us</a>
+          <a
+            href="https://www.instagram.com/artandpoeticjustice/?hl=en"
+            className={styles.links}
+          >
+            slow-skate with us
+          </a>
           <button>look here</button>
           <FilmList />
         </main>
--- src/components/splash.js
+++ src/components/splash.js
@@ -4,26 +4,23 @@ import styles from './splash.module.scss'
 
 export default function Splash(props) {
   if (props.isVisible) {
-    // get viewport size to calc outer video size (w,h) & then calc w,h inner video will be based on viewport size
-    const viewportWidth = 0
-    const viewportHeight = 0
-    const innerVideoWidth = 0
-    const innerVideoHeight = 0
-
     return (
       <div className={styles.splash}>
-        <video autoPlay loop muted width={viewportWidth} height={viewportHeight} className={styles.outer}>
-          <source src={props.video + ".webm"} type="video/webm" />
-          <source src={props.video + ".mp4"} type="video/mp4" />
+        <video autoPlay loop muted width="100%" height="100%">
+          <source src={props.video + '.webm'} type="video/webm" />
+          <source src={props.video + '.mp4'} type="video/mp4" />
           Sorry, your browser doesn't support embedded videos.
         </video>
-        <div className={styles.innerVideoWrapper}>
-          <video autoPlay loop muted width={innerVideoWidth} height={innerVideoHeight} className={styles.inner}>
-            <source src={props.video + ".webm"} type="video/webm" />
-            <source src={props.video + ".mp4"} type="video/mp4" />
+        <div className={styles.inner}>
+          <video autoPlay loop muted width="100%" height="100%">
+            <source src={props.video + '.webm'} type="video/webm" />
+            <source src={props.video + '.mp4'} type="video/mp4" />
             Sorry, your browser doesn't support embedded videos.
           </video>
-          <AudioButton className={styles.audioButton} getIconPos={props.getIconPos} />
+          <AudioButton
+            className={styles.audioButton}
+            getIconPos={props.getIconPos}
+          />
           {/* generic tags can't be inside video elements, audio tag had to be moved out */}
         </div>
       </div>
