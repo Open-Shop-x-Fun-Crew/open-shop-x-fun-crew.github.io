37a826c827e0c419b10990ab58dc26ee6c2a5f6d
Emmy_codes <alunia.moon@gmail.com>
2020-12-29 12:31:30 +0100

    Finishing touchest on HTML

--- src/components/film-list-item.js
+++ src/components/film-list-item.js
@@ -6,5 +6,6 @@ export default function FilmListItem(props) {
   return <section className={styles.filmstripSection}>
     <img src={props.image} alt={props.alt} />
     <p className={styles.filmstripText}>{props.text}</p>
+    <p className={styles.filmstripText}>{props.description}</p>
   </section>
 }
--- src/components/film-list.js
+++ src/components/film-list.js
@@ -9,9 +9,9 @@ import styles from './film-list.module.scss'
 export default function FilmList(props) {
   return (
     <ul>
-      <FilmListItem image={film1} text="Distressed" alt="Description of film" />
-      <FilmListItem image={film2} text="Dimmiga Dar" alt="Description of film" />
-      <FilmListItem image={film3} text="Mad Dog - 2016" alt="Description of film" />
+      <FilmListItem image={film1} text="Distressed" description="Short film 2020, 15min" alt="Description of film" />
+      <FilmListItem image={film2} text="Dimmiga Dar" descrption="Short film 2020, 17min" alt="Description of film" />
+      <FilmListItem image={film3} text="Mad Dog" description="Full-length film 2016, 85min" alt="Description of film" />
     </ul>
   )
 }
