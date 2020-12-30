b994901551c8f0f1ac44f34e7587409162571810
nehahirve <neha.hirve@hyperisland.se>
2020-12-30 13:24:54 +0100

    footer css

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,21 +3,25 @@ import FilmList from './film-list'
 import martin from '../../static/images/martin.png'
 import viktor from '../../static/images/viktor.png'
 import styles from './footer.module.scss'
-import buttonStyles from './logo-button.module.scss'
 
 export default function Footer(props) {
   if (props.isVisible) {
     return (
       <footer>
-        <p>open shop Nytorgsgatan 20 117 54 Stockholm info@openshop.se</p>
-        <a
-          className={`${buttonStyles.button} ${styles.logo} ${styles.links}`}
-          href="#top"
-        >
+        <address>
+          open shop
+          <br />
+          Nytorgsgatan 20
+          <br />
+          117 54 Stockholm
+          <br />
+          info@openshop.se
+        </address>
+        <button className={styles.logo}>
           open
           <br />
           shop.
-        </a>
+        </button>
       </footer>
     )
   } else {
