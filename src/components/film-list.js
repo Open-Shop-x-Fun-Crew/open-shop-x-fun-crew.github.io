e97af3f13caae79d427c21f83c6b556769909f60
nehahirve <neha.hirve@hyperisland.se>
2020-12-31 10:34:44 +0100

    films load from json file, we have correct screenshots

--- src/components/film-list.js
+++ src/components/film-list.js
@@ -1,34 +1,24 @@
 import React from 'react'
 import FilmListItem from './film-list-item'
-import Distressed from '../../static/images/Distressed.jpg'
-import DimmigaDar from '../../static/images/DimmigaDar.jpg'
-import MadDog from '../../static/images/MadDog.jpg'
 
 import styles from './film-list.module.scss'
 
+import films from '../films/films.json'
+
 export default function FilmList(props) {
-  if (props.isVisible) {
+  const filmListItems = films.map(film => {
+    console.log(film.filmScreenshot)
     return (
-      <ul id="film-list">
-        <FilmListItem
-          image={Distressed}
-          text="Distressed"
-          description="Short film 2020. 15min"
-          alt="Description of film"
-        />
-        <FilmListItem
-          image={DimmigaDar}
-          text="Dimmiga Dar"
-          description="Short film 2019. 17min"
-          alt="Description of film"
-        />
-        <FilmListItem
-          image={MadDog}
-          text="Mad Dog"
-          description="Documentary 2016. 85min"
-          alt="Description of film"
-        />
-      </ul>
+      <FilmListItem
+        image={`screenshots/${film.filmScreenshot}`}
+        title={film.filmTitle}
+        description={film.filmDescription}
+        alt={film.filmTagline}
+      />
     )
+  })
+
+  if (props.isVisible) {
+    return <ul id="film-list">{filmListItems}</ul>
   } else return null
 }
