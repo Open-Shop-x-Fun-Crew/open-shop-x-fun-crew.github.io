ac45ebd4edbedc996d9d5782a0a912e57e15edf8
nehahirve <neha.hirve@hyperisland.se>
2021-01-04 14:06:17 +0100

    refactor

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,34 +3,30 @@ import styles from './footer.module.scss'
 import { Link } from 'react-scroll'
 
 export default function Footer(props) {
-  if (props.isVisible) {
-    return (
-      <footer>
-        <address>
-          open shop
+  return (
+    <footer>
+      <address>
+        open shop
+        <br />
+        Nytorgsgatan 20
+        <br />
+        117 54 Stockholm
+        <br />
+        info@openshop.se
+      </address>
+      <Link
+        activeClass="active"
+        to="info"
+        spy={true}
+        smooth={true}
+        duration={500}
+      >
+        <div className={styles.logo}>
+          open
           <br />
-          Nytorgsgatan 20
-          <br />
-          117 54 Stockholm
-          <br />
-          info@openshop.se
-        </address>
-        <Link
-          activeClass="active"
-          to="info"
-          spy={true}
-          smooth={true}
-          duration={500}
-        >
-          <div className={styles.logo}>
-            open
-            <br />
-            shop.
-          </div>
-        </Link>
-      </footer>
-    )
-  } else {
-    return null
-  }
+          shop.
+        </div>
+      </Link>
+    </footer>
+  )
 }
