58b4da1a39cba0362109a0ec7c06915a81cad64e
nehahirve <neha.hirve@hyperisland.se>
2021-01-04 22:02:21 +0100

    removed react-video player

--- src/components/splash.js
+++ src/components/splash.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react'
-import ReactPlayer from 'react-player'
 import AudioButton from '../components/audio-button.js'
 import styles from './splash.module.scss'
 import styled, { keyframes } from 'styled-components'
@@ -11,7 +10,7 @@ const backgrounds = filmInfo.splashVideos
 function getRandom(number, previousNumber) {
   let randomNumber = Math.floor(Math.random() * number) + 1
   if (randomNumber === previousNumber) {
-    return getRandom(number, previousNumber)
+    return getRandom(number, randomNumber)
   } else return randomNumber
 }
 
@@ -38,6 +37,10 @@ const ExpandingDiv = styled.div`
 export default function Splash(props) {
   const [startVid, setStartVid] = useState(getRandom(backgrounds))
 
+  function test() {
+    console.log('hey')
+  }
+
   function next() {
     if (+startVid === backgrounds) {
       setStartVid(1)
@@ -49,24 +52,37 @@ export default function Splash(props) {
   return (
     <ExpandingDiv animationCenter={props.animationCenter}>
       <div className={styles.splash} style={{ zIndex: props.onTop ? 90 : 0 }}>
-        <ReactPlayer
+        <video
+          type="video/mp4"
           className={styles.bgVid}
+          src={`/splashvids/bg_${startVid}.mp4`}
+          autoPlay={true}
+          onEnded={next}
+          preload="auto"
           muted={true}
+          playsInline={true}
           width="100%"
           height="100%"
-          url={`/splashvids/bg_${startVid}.mp4`}
-          playing={true}
-          onEnded={next}
-        />
+        ></video>
 
         <div className={styles.inner}>
-          <ReactPlayer
+          {/* <ReactPlayer
             className={styles.mainVid}
             muted={true}
             url={`/splashvids/fg_${startVid}.mp4`}
             playing={true}
-            onEnded={next}
-          />
+            // onEnded={next}
+          /> */}
+          <video
+            type="video/mp4"
+            className={styles.mainVid}
+            src={`/splashvids/fg_${startVid}.mp4`}
+            autoPlay={true}
+            preload="metadata"
+            muted={true}
+            poster={`screenshots/mad-dog.png`}
+            playsInline={true}
+          ></video>
           <AudioButton
             className={styles.audioButton}
             getIconPos={props.getIconPos}
