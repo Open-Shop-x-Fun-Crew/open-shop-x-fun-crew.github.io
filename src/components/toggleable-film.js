8b3e0a04ca43b3637b96a89c6b250a37f44626a7
Sofia Darke <sofia.darke@hyperisland.se>
2021-01-05 15:24:57 +0100

    Player added that open trailers and close them

--- src/components/toggleable-film.js
+++ src/components/toggleable-film.js
@@ -4,15 +4,32 @@ import Player from '../components/player'
 import styles from './toggleable-film.module.scss'
 
 export default function ToggleableFilm(props) {
+  console.log(props.title)
   const [playing, setPlaying] = useState(false)
 
+  function togglePlayer() {
+    setPlaying(!playing)
+  }
+
   if (playing) {
-    return <Player />
+    return (
+      <Player
+        url={props.url}
+        togglePlayer={togglePlayer}
+        title={props.title}
+        description={props.description}
+      />
+    )
   }
 
   return (
     <li className={styles.filmstrip}>
-      <img src={props.image} alt={props.alt} className={styles.image} />
+      <img
+        src={props.image}
+        alt={props.alt}
+        className={styles.image}
+        onClick={togglePlayer}
+      />
       <div className={styles.info}>
         <p className={styles.title}>{props.title}</p>
         <p className={styles.description}>{props.description}</p>
