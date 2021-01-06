aff343d4a3cece16f9daba6f2c92339de3c5a3aa
nehahirve <neha.hirve@hyperisland.se>
2021-01-06 18:20:07 +0100

    Revert "Merge pull request #25 from Open-Shop-x-Fun-Crew/one-video-at-a-time-please"
    This reverts commit d1c20b0a923ab395e60840ae339d5430fd8a13a2, reversing
    changes made to 372c11ed2d158a1e12815f5915473554c0fe69b4.

--- src/components/toggleable-film.js
+++ src/components/toggleable-film.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Player from '../components/player'
 
 import styles from './toggleable-film.module.scss'
 
 export default function ToggleableFilm(props) {
   console.log(props.title)
+  const [playing, setPlaying] = useState(false)
+
+  function togglePlayer() {
+    setPlaying(!playing)
+  }
+
+  if (playing) {
+    return (
+      <Player
+        url={props.url}
+        togglePlayer={togglePlayer}
+        title={props.title}
+        description={props.description}
+      />
+    )
+  }
 
   return (
     <li className={styles.filmstrip}>
@@ -11,7 +28,7 @@ export default function ToggleableFilm(props) {
         src={props.image}
         alt={props.alt}
         className={styles.image}
-        onClick={props.togglePlayer}
+        onClick={togglePlayer}
       />
       <div className={styles.info}>
         <p className={styles.title}>{props.title}</p>
