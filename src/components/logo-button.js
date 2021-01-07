ccabb5b36a60cbe8faa3b26b0ea47b81d843cae3
nehahirve <neha.hirve@hyperisland.se>
2021-01-07 13:19:49 +0100

    removed audio button

--- src/components/logo-button.js
+++ src/components/logo-button.js
@@ -3,7 +3,6 @@ import styles from './logo-button.module.scss'
 
 export default function LogoButton(props) {
   const [pos, setPos] = useState({ x: null, y: null })
-  const [overlapping, setOverlapping] = useState(true)
 
   function onClick(e) {
     props.onClick({ x: pos.x, y: pos.y })
@@ -12,20 +11,6 @@ export default function LogoButton(props) {
   function updatePos(e) {
     if (props.splashVisible) {
       setPos({ x: e.clientX, y: e.clientY })
-      checkIfOverlapping(e)
-    }
-  }
-
-  function checkIfOverlapping(e) {
-    if (props.audioIconPos) {
-      if (
-        e.clientX > props.audioIconPos.x &&
-        e.clientX < props.audioIconPos.x + props.audioIconPos.w &&
-        e.clientY > props.audioIconPos.y &&
-        e.clientY < props.audioIconPos.y + props.audioIconPos.h
-      ) {
-        setOverlapping(true)
-      } else setOverlapping(false)
     }
   }
 
@@ -34,19 +19,17 @@ export default function LogoButton(props) {
     return () => window.removeEventListener('mousemove', updatePos)
   }, [props.splashVisible])
 
-  if (!overlapping) {
-    return (
-      <button
-        className={`${styles.button} ${
-          props.splashVisible ? styles.moving : styles.sticky
-        }`}
-        onClick={onClick}
-        style={{ top: pos.y, left: pos.x }}
-      >
-        open
-        <br />
-        shop.
-      </button>
-    )
-  } else return null
+  return pos.x ? (
+    <button
+      className={`${styles.button} ${
+        props.splashVisible ? styles.moving : styles.sticky
+      }`}
+      onClick={onClick}
+      style={{ top: pos.y, left: pos.x }}
+    >
+      open
+      <br />
+      shop.
+    </button>
+  ) : null
 }
