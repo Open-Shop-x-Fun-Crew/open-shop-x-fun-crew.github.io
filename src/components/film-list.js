783f23b858754c0c0fa70e33470f92417bc36471
nehahirve <neha.hirve@hyperisland.se>
2021-01-07 11:37:03 +0100

    scroll to video on click works

--- src/components/film-list.js
+++ src/components/film-list.js
@@ -7,23 +7,25 @@ import filmInfo from '../films/films.json'
 
 export default function FilmList(props) {
   const [currentTrailer, setCurrentTrailer] = useState('Mad Dog')
-  
+
   function togglePlayer(e, title) {
     setCurrentTrailer(title)
   }
 
   const filmListItems = filmInfo.films.map(film => {
     return (
-      <ToggleableFilm
-        currentTrailer={currentTrailer}
-        togglePlayer={togglePlayer}
-        key={filmInfo.films.indexOf(film)}
-        image={`screenshots/${film.filmScreenshot}`}
-        title={film.filmTitle}
-        description={film.filmDescription}
-        alt={film.filmTagline}
-        url={film.filmURL}
-      />
+      <div id={film.filmURL}>
+        <ToggleableFilm
+          currentTrailer={currentTrailer}
+          togglePlayer={togglePlayer}
+          key={filmInfo.films.indexOf(film)}
+          image={`screenshots/${film.filmScreenshot}`}
+          title={film.filmTitle}
+          description={film.filmDescription}
+          alt={film.filmTagline}
+          url={film.filmURL}
+        />
+      </div>
     )
   })
 
--- src/components/toggleable-film.js
+++ src/components/toggleable-film.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react'
 import Player from '../components/player'
+import { Link } from 'react-scroll'
 
 import styles from './toggleable-film.module.scss'
 
 export default function ToggleableFilm(props) {
-
+  console.log(props.id, props.url)
   if (props.title === props.currentTrailer) {
     return (
       <Player
@@ -16,17 +17,26 @@ export default function ToggleableFilm(props) {
   }
 
   return (
-    <li className={styles.filmstrip}>
-      <img
-        src={props.image}
-        alt={props.alt}
-        className={styles.image}
-        onClick={(e) => props.togglePlayer(e, props.title)}
-      />
-      <div className={styles.info}>
-        <p className={styles.title}>{props.title}</p>
-        <p className={styles.description}>{props.description}</p>
-      </div>
-    </li>
+    <Link
+      activeClass="active"
+      to={props.url}
+      spy={true}
+      smooth={true}
+      duration={500}
+    >
+      <li className={styles.filmstrip}>
+        <img
+          src={props.image}
+          alt={props.alt}
+          className={styles.image}
+          onClick={e => props.togglePlayer(e, props.title)}
+        />
+
+        <div className={styles.info}>
+          <p className={styles.title}>{props.title}</p>
+          <p className={styles.description}>{props.description}</p>
+        </div>
+      </li>
+    </Link>
   )
 }
