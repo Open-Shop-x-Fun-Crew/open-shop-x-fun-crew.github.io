603ec00d9c99944e9390e8dd30feefa55c15beb6
Emma Scott-Allen <alunia.moon@gmail.com>
2021-01-07 10:07:27 +0100

    one film at a time playing

--- src/components/film-list.js
+++ src/components/film-list.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ToggleableFilm from './toggleable-film'
 
 import styles from './film-list.module.scss'
@@ -6,9 +6,17 @@ import styles from './film-list.module.scss'
 import filmInfo from '../films/films.json'
 
 export default function FilmList(props) {
+  const [currentTrailer, setCurrentTrailer] = useState('Mad Dog')
+  
+  function togglePlayer(e, title) {
+    setCurrentTrailer(title)
+  }
+
   const filmListItems = filmInfo.films.map(film => {
     return (
       <ToggleableFilm
+        currentTrailer={currentTrailer}
+        togglePlayer={togglePlayer}
         key={filmInfo.films.indexOf(film)}
         image={`screenshots/${film.filmScreenshot}`}
         title={film.filmTitle}
