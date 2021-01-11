002e152913342e42e43dfb16a39579ff8ee19e35
emmy-codes <70635859+emmy-codes@users.noreply.github.com>
2021-01-11 22:29:32 +0000

    Deploying to master from  @ 96e23675fce19e1b6e4901471dba238bc49febb7 🚀

--- gatsby-browser.js
+++
@@ -1,7 +0,0 @@
-import './src/styles/reset.css'
-import './src/styles/global.scss'
-
-export const onRouteUpdate = ({ location, prevLocation }) => {
-  if (location && location.state)
-    location.state.referrer = prevLocation ? prevLocation.pathname : null
-}
