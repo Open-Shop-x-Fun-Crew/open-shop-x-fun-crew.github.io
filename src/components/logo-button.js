30e52f1e05a31ed24082266b13bf57b0e21addf7
nehahirve <neha.hirve@hyperisland.se>
2021-01-13 22:09:07 +0100

    changed logo pos

--- src/components/logo-button.js
+++ src/components/logo-button.js
@@ -1,11 +1,16 @@
 import React, { useState, useEffect } from 'react'
 import styles from './logo-button.module.scss'
+import { isMobile } from 'react-device-detect'
 
 export default function LogoButton(props) {
-  const [pos, setPos] = useState({
-    x: 200,
-    y: 200,
-  })
+  const [pos, setPos] = useState(
+    isMobile
+      ? { x: window.innerWidth - 60, y: 60 }
+      : {
+          x: 200,
+          y: 200,
+        }
+  )
 
   function onClick(e) {
     props.onClick({ x: pos.x, y: pos.y })
